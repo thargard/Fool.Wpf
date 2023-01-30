2cfea3f3fcc9140cdbb9a0dd321cecc3ab73ed5a
thargard <boris.margolin.1301@gmail.com>
2023-01-30 18:55:56 +0300

    "take 1 card" and "deal 4 cards" buttons added

--- Fool.Wpf/MainWindow.xaml.cs
+++ Fool.Wpf/MainWindow.xaml.cs
@@ -27,16 +27,34 @@ namespace Fool.Wpf
 
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
-            /*var but = new Button() { Content = "Дама пик", Height = 20, Width = 80 };
+            var but = new Button() { Content = "Дама пик", Height = 20, Width = 80 };
             but.Click += But_Click;
-            _grid.Children.Add(but);*/
+            _grid.Children.Add(but);
         }
 
         private void But_Click(object sender, RoutedEventArgs e)
         {
-            /*var but = new Button() { Content = "Дама треф", Height = 30, Width = 70 };
+            var but = new Button() { Content = "Дама треф", Height = 30, Width = 70 };
             but.Click += But_Click;
-            _grid.Children.Add(but);*/
+            _grid.Children.Add(but);
+        }
+
+        private void OneCard(object sender, RoutedEventArgs e)
+        {
+            var but = new Button() { Content = "Дама пик", Height = 40, Width = 70 };
+            _handArea.Children.Add(but);
+        }
+
+        private void StartGame(object sender, RoutedEventArgs e)
+        {
+            var but = new Button() { Content = "Дама пик", Height = 40, Width = 70 };
+            _handArea.Children.Add(but);
+            var but1 = new Button() { Content = "Дама треф", Height = 40, Width = 70 };
+            _handArea.Children.Add(but1);
+            var but2 = new Button() { Content = "Дама пик", Height = 40, Width = 70 };
+            _handArea.Children.Add(but2);
+            var but3 = new Button() { Content = "Дама пик", Height = 40, Width = 70 };
+            _handArea.Children.Add(but3);
         }
     }
 }
