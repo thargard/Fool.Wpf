7cc7868489b75d61c20a8068bdd36f38e43a5275
Ilya <marhoily@hotmail.com>
2023-02-02 15:35:31 +0300

    cleanup

--- Fool.Web/Controllers/CommonState.cs
+++ Fool.Web/Controllers/CommonState.cs
@@ -1,6 +1,7 @@
-namespace Fool.Web.Controllers;
+﻿namespace Fool.Web.Controllers;
 
-public static class CommonState
+public static class CommonState // "имя"
 {
+    // глобальная переменная
     public static SharedGameState SharedState { get; set; }
 }
--- Fool.Wpf/MainWindow.xaml.cs
+++ Fool.Wpf/MainWindow.xaml.cs
@@ -84,8 +84,7 @@ namespace Fool.Wpf
 
             var card = HttpUtility.UrlEncode("дама пик");
             var str = $"https://localhost:7081/Move?card={card}";
-            var str1 = await httpClient.PostAsJsonAsync(
-                str, 0);
+            var str1 = await httpClient.PostAsJsonAsync(str, 0);
             str1.EnsureSuccessStatusCode();
 
         }
