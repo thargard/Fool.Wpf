a8d8169da8ad5f6897706dd097ce2f7cb876c402
GMargolin <91896446+GMargolin@users.noreply.github.com>
2023-02-11 20:00:03 +0300

    comment

--- Fool.Web/Controllers/TakeCardController.cs
+++ Fool.Web/Controllers/TakeCardController.cs
@@ -15,9 +15,10 @@ public class TakeCardController : ControllerBase
             return false;
         Player player1 = CommonState.SharedState.Players.Single(p => p.Id == playerId);
         player1.Hand.Add(CommonState.SharedState.GetOneCard());
-        CommonState.SharedState.LastTurnTime= DateTime.Now;
+
+        /*CommonState.SharedState.LastTurnTime= DateTime.Now;
         if (player1.Id == 1) CommonState.SharedState.CurrentMovePlayerId = 2;
-        else CommonState.SharedState.CurrentMovePlayerId = 1;
+        else CommonState.SharedState.CurrentMovePlayerId = 1;*/
         return true;
     }
 }
